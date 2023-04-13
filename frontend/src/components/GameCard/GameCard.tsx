56e94af1909453cdb827aedfb704ccb3c7246b00
Krysset <maxhagman@hotmail.se>
2023-04-13 10:24:14 +0200

    Made frontend show platform of games

--- frontend/src/components/GameCard/GameCard.tsx
+++ frontend/src/components/GameCard/GameCard.tsx
@@ -4,13 +4,15 @@ import styles from './GameCard.module.css';
 interface GameCardProps {
 	name: string;
 	description: string;
+	platform: string;
 }
 
-const GameCard: FC<GameCardProps> = ({ name, description }) => {
+const GameCard: FC<GameCardProps> = ({ name, description, platform }) => {
 	return (
 		<li className={styles.card}>
 			<h2>{name}</h2>
 			<p>{description}</p>
+			<p>Platform: {platform}</p>
 		</li>
 	);
 };
