b9a12c25ff51d6c0f0a30bd9c906ae9e34d8df23
Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>
2023-04-17 14:30:58 +0200

    Added ability to get platforms from backend

---
+++ backend/src/routers/platformRouter.ts
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+import { getAllPlatforms } from '../services/platformService';
+
+const platformRouter = Router();
+
+/**
+ * @api {get} /api/v1/platforms Request Platforms
+ * @apiName GetPlatforms
+ * @apiGroup Platforms
+ * @apiDescription Get all platforms
+ *
+ * @apiSuccess {Object[]} platforms List of platforms
+ *
+ * @apiSuccessExample Success-Response:
+ * HTTP/1.1 200 OK
+ * [
+ *  {
+ *    name: "Steam"
+ *  }
+ * ]
+ */
+platformRouter.get('/', async (req, res) => {
+	const platforms = await getAllPlatforms();
+
+	res.status(200).json(platforms);
+});
