ef6d0529d59f1105e88e779c3b265b92adbdda00
Dennis Christensen <dennis.f.christensen@gmail.com>
2023-04-18 15:49:34 +0200

    Added the searchbar

--- frontend/src/components/GamesList/GamesList.tsx
+++ frontend/src/components/GamesList/GamesList.tsx
@@ -1,7 +1,8 @@
 import { useApiGet } from '@/src/hooks/apiHooks';
-import { FC } from 'react';
+import { ChangeEvent, FC } from 'react';
 import GameCard from '../GameCard/GameCard';
 import styles from './GamesList.module.css';
+import debounce from 'lodash.debounce';
 
 interface GamesListProps {}
 
@@ -17,9 +18,19 @@ interface Game {
 const GamesList: FC<GamesListProps> = () => {
 	const { data, error, loading } = useApiGet<Game[]>('/games');
 
+	const search = debounce((e: ChangeEvent) => {
+		console.log(e.target.value)
+		// TODO: send a search request to the backend and update data
+	}, 300)
+
 	return (
-		<div>
-			<h1 className={styles.gamesListHeader}>Games List</h1>
+		<div style={{width: 'auto'}}>
+			<input 
+				className={styles.gamesListSearchBar} 
+				type='text'
+				placeholder='Search for a game'
+				onChange={search}
+			/>
 
 			{loading ? <p>Loading...</p> : null}
 
