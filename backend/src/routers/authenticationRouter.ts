41b3da56816174ce0dbe6fe2e44e325eb727ce56
Oscar Eriksson <oscariremma@gmail.com>
2023-04-21 18:03:51 +0200

    Add apidoc
    Co-authored-by: Goostaf <gasplund2@gmail.com>

--- backend/src/routers/authenticationRouter.ts
+++ backend/src/routers/authenticationRouter.ts
@@ -7,7 +7,39 @@ import { createAccount } from '../services/accountService.js';
 
 const authRouter = Router();
 
+/**
+ * @api {get} api/v1/auth/login Gamma login
+ * @apiName Login
+ * @apiGroup Authentication
+ * @apiDescription Redirects the user to log in via Gamma
+ *
+ * @apiSuccess (302) redirect Redirects the user to Gamma
+ *
+ */
 authRouter.get('/login', passport.authenticate('gamma'));
+
+/**
+ * @api {get} api/v1/auth/callback Gamma callback
+ * @apiName LoginCallback
+ * @apiGroup Authentication
+ * @apiDescription Callback from Gamma for user login
+ * @apiQuery {String} code Code from Gamma
+ *
+ * @apiSuccess {Object} GammaUser Information about the logged in user
+ *
+ * @apiSuccessExample Success-Response:
+ * HTTP/1.1 200 OK
+ * {
+ *     "cid": "admin",
+ *     "is_admin": false,
+ *     "groups": [
+ *         "superadmin",
+ *         "admin"
+ *     ],
+ *     "language": "sv",
+ *     "phone": null
+ * }
+ */
 authRouter.get(
 	'/callback',
 	passport.authenticate('gamma'),
@@ -32,6 +64,21 @@ authRouter.get(
         res.status(200).json(user);
 	}
 );
+
+/**
+ * @api {post} api/v1/auth/logout Logout
+ * @apiName Logout
+ * @apiGroup Authentication
+ * @apiDescription Logs out the current user
+ *
+ * @apiSuccess {json} message Message indicating success
+ *
+ * @apiSuccessExample Success-Response:
+ * HTTP/1.1 200 OK
+ * {
+ * 	"message": "Logged out"
+ * }
+ */
 authRouter.post('/logout', (req: express.Request, res: express.Response) => {
 	req.logOut((err) => {
 		// TODO: Better error handling
@@ -45,6 +92,36 @@ authRouter.post('/logout', (req: express.Request, res: express.Response) => {
 	});
 });
 
+
+/**
+ * @api {get} api/v1/auth/user Get user
+ * @apiName GetUser
+ * @apiGroup Authentication
+ * @apiDescription Returns information about the logged in user
+ *
+ * @apiSuccess {Object} GammaUser Information about the user
+ *
+ * @apiSuccessExample Success-Response:
+ * HTTP/1.1 200 OK
+ * {
+ *     "cid": "admin",
+ *     "is_admin": false,
+ *     "groups": [
+ *         "superadmin",
+ *         "admin"
+ *     ],
+ *     "language": "sv",
+ *     "phone": null
+ * }
+ *
+ * @apiError (401) NotLoggedIn User is not logged in
+ * @apiErrorExample {json} Error-Response:
+ * HTTP/1.1 401 Unauthorized
+ * {
+ *    "message": "Not logged in"
+ * }
+ * 
+ */
 authRouter.get('/user', (req: express.Request, res: express.Response) => {
     if (req.isAuthenticated()) {
         const user: GammaUser = {
