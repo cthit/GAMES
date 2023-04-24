1516d09eb5974e4d216277381c455e91a48cfb84
Oscar Palm <55193662+x183@users.noreply.github.com>
2023-04-24 10:05:31 +0200

    Fix/borrowing rules (#59)
    * Added rules to stop the most outrageous types of borrowing
    
    * Changed the error messages returned when sending faulty borrow requests to conform with the coding standard

--- backend/src/routers/borrowRouter.ts
+++ backend/src/routers/borrowRouter.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
 import { z } from 'zod';
 import { validateRequestBody } from 'zod-express-middleware';
-import { borrowGame, returnGame } from '../services/borrowService.js';
+import { BorrowStatus, borrowGame, returnGame } from '../services/borrowService.js';
+import sendApiValidationError from '../utils/sendApiValidationError.js';
 
 const borrowRouter = Router();
 
@@ -35,12 +36,24 @@ const borrowSchema = z.object({
  */
 borrowRouter.post('/', validateRequestBody(borrowSchema), async (req, res) => {
 	const body = req.body;
-	await borrowGame(
-		body.gameId,
-		body.user,
-		new Date(body.borrowStart),
-		new Date(body.borrowEnd)
-	);
+	const status = await borrowGame(
+			body.gameId,
+			body.user,
+			new Date(body.borrowStart),
+			new Date(body.borrowEnd)
+		);
+	if (status == BorrowStatus.Borrowed) return sendApiValidationError(res,
+		{
+			path: 'gameId',
+			message: 'The game is already borrowed'
+		},
+		'Body');
+	if (status == BorrowStatus.NotValid) return sendApiValidationError(res,
+		{
+			path: 'gameId',
+			message: 'The gameId given is not valid'
+		},
+		'Body');
 	res.status(200).json({ message: 'Game successfully borrowed' });
 });
 
@@ -72,8 +85,20 @@ borrowRouter.post(
 	'/return',
 	validateRequestBody(returnSchema),
 	async (req, res) => {
-		const body = req.body;
-		await returnGame(body.gameId, body.user);
+			const body = req.body;
+		const status = await returnGame(body.gameId, body.user);
+		if (status == BorrowStatus.NotBorrowed) return sendApiValidationError(res,
+			{
+				path: 'gameId',
+				message: 'The game isn\'t borrowed and can therefore not be returned'
+			},
+			'Body');
+		if (status == BorrowStatus.NotValid) return sendApiValidationError(res,
+			{
+				path: 'gameId',
+				message: 'The gameId given is not valid'
+			},
+			'Body');
 		res.status(200).json({ message: 'Game successfully returned' });
 	}
 );
