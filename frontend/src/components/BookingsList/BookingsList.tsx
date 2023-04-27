80960502bd8e7d5210a291b5ed29a88555e015fe
Goostaf <gasplund2@gmail.com>
2023-04-27 15:42:54 +0200

    Bookings list (#67)
    * API endpoint for getting booked borrows
    
    * Include game name properly
    
    * Add bookings UI
    
    * Proper naming and docs

---
+++ frontend/src/components/BookingsList/BookingsList.tsx
@@ -0,0 +1,43 @@
+import { useApiGet } from '@/src/hooks/apiHooks';
+import { ChangeEvent, FC, useState } from 'react';
+import styles from './BookingsList.module.css';
+import BookingCard from '../BookingCard/BookingCard';
+
+interface BookingsListProps {}
+
+interface Booking {
+    gameName: string,
+    user: string,
+    borrowStart: string,
+    borrowEnd: string
+}
+
+const BookingsList: FC<BookingsListProps> = () => {
+	const [apiPath, setApiPath] = useState("/borrow/list")
+	const { data, error, loading } = useApiGet<Booking[]>(apiPath);
+
+	return (
+		<div style={{width: 'auto'}}>
+
+			{loading ? <p>Loading...</p> : null}
+
+			{error ? <p>Error: {error}</p> : null}
+
+			{data ? (
+				<ul className={styles.bookingsList}>
+					{data.map((booking) => (
+						<BookingCard
+							key={`${booking.gameName}.${booking.borrowStart}.${booking.borrowEnd}`}
+							game={booking.gameName}
+                            user={booking.user}
+                            start={booking.borrowStart}
+                            end={booking.borrowEnd}
+						/>
+					))}
+				</ul>
+			) : null}
+		</div>
+	);
+};
+
+export default BookingsList;
---
+++ frontend/src/pages/bookings.tsx
@@ -0,0 +1,29 @@
+import { Inter } from 'next/font/google';
+import Head from 'next/head';
+import BookingsList from '../components/BookingsList/BookingsList';
+import Header from '../components/Header/Header';
+import styles from './index.module.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const Bookings = () => {
+	return (
+		<>
+			<Head>
+				<title>GAMES | Bookings</title>
+				<meta
+					name="description"
+					content="A service for finding and browsing games in different mediums and formats."
+				/>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<Header />
+			<main className={styles.mainContainer}>
+				<BookingsList />
+			</main>
+		</>
+	);
+};
+
+export default Bookings;
