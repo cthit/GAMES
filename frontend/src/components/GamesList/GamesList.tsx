ae86086f5515fcfb13fd124aa28f51b09fe5a3c5
Oscar Palm <55193662+x183@users.noreply.github.com>
2023-05-04 15:27:20 +0200

    Fix/filter playtime range (#81)
    * Most parts of removing a game.
    
    * Added game removal to frontend
    
    * Added checks to games/remove so that we don't remove borrowed games
    
    * Reloads page, even if game not removed, can't seem to fix it, otherwise works as intended now
    
    * commented problems for future me to fix
    
    * Changed to filter on playtimerange instead of on a number
    
    * Add organizations backend (Organizations part 1) (#68)
    
    * Initial stage of adding Gamma logon
    
    Most Gamma specific code comes from https://github.com/cthit/bookIT-node
    
    Co-authored-by: Goostaf <gasplund2@gmail.com>
    
    * Add working gamma login system
    
    
    
    Co-authored-by: Goostaf <gasplund2@gmail.com>
    Co-authored-by: Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>
    
    * Add apidoc
    
    
    Co-authored-by: Goostaf <gasplund2@gmail.com>
    
    * Fix merge error
    
    * Update frontend/src/pages/callback.tsx
    
    Co-authored-by: Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>
    
    * Add Organizations to schema
    
    
    Co-authored-by: Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>
    
    * Refactor Gamma code
    
    
    Co-authored-by: Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>
    
    * Add Gamma API service
    
    
    Co-authored-by: Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>
    
    * Clean app.ts
    
    
    Co-authored-by: Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>
    
    * Start of adding users to orgs on login
    
    
    Co-authored-by: Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>
    
    * Start of site admin page
    
    
    Co-authored-by: Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>
    
    * Add users to orgs based on gamma
    
    * Add site admin org management backend
    
    * Fix merge error
    
    * Remove dedicated admin table. Use field on member instead
    
    * Remove new lines
    
    * Remove console log
    
    Also includes a prettier format of passport.ts
    
    * API doc half done
    
    * Added remaining API docs
    
    * Add mock env to enable superadmin access
    
    * Remove duplicated file
    
    * Use Promise.All instead of loop. Throw error when not handled
    
    ---------
    
    Co-authored-by: Goostaf <gasplund2@gmail.com>
    Co-authored-by: Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>
    
    * Feature/#54 seeder file (#65)
    
    * added seeder script file
    
    * added pnpm command
    
    * Feature/delete games (#80)
    
    * Most parts of removing a game.
    
    * Added game removal to frontend
    
    * Added checks to games/remove so that we don't remove borrowed games
    
    * Reloads page, even if game not removed, can't seem to fix it, otherwise works as intended now
    
    * commented problems for future me to fix
    
    * Update backend/src/services/gameService.ts
    
    Yes, reasonable
    
    Co-authored-by: Max Hagman <maxhagman@hotmail.se>
    
    ---------
    
    Co-authored-by: EmilMadsstuen <emil.madsstuen@gmail.com>
    Co-authored-by: Max Hagman <maxhagman@hotmail.se>
    
    * Can add games before public release (#85)
    
    * Resolved conversations
    
    ---------
    
    Co-authored-by: EmilMadsstuen <emil.madsstuen@gmail.com>
    Co-authored-by: Oscar Eriksson <oscariremma@gmail.com>
    Co-authored-by: Goostaf <gasplund2@gmail.com>
    Co-authored-by: Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>
    Co-authored-by: Dennis-coder <dennis.f.christensen@gmail.com>
    Co-authored-by: Max Hagman <maxhagman@hotmail.se>

--- frontend/src/components/GamesList/GamesList.tsx
+++ frontend/src/components/GamesList/GamesList.tsx
@@ -26,7 +26,8 @@ type SearchFilter = {
 	platform?: string;
 	releaseBefore?: Date;
 	releaseAfter?: Date;
-	playtime?: number;
+	playtimeMax?: number;
+	playtimeMin?: number;
 	playerCount?: number;
 	location?: string; 
 	owner?: string;
@@ -39,7 +40,8 @@ const GamesList: FC<GamesListProps> = () => {
 	const [platform, setPlatform] = useState('');
 	const [releaseBefore, setReleaseBefore] = useState<Date>();
 	const [releaseAfter, setReleaseAfter] = useState<Date>();
-	const [playtime, setPlaytime] = useState<number>();
+	const [playtimeMax, setPlaytimeMax] = useState<number>();
+	const [playtimeMin, setPlaytimeMin] = useState<number>();
 	const [playerCount, setPlayerCount] = useState<number>();
 	const [owner, setOwner] = useState<string>();
 	const searchFilter: SearchFilter = {};
@@ -52,7 +54,8 @@ const GamesList: FC<GamesListProps> = () => {
 		if (platform) searchFilter.platform = platform;
 		if (releaseBefore) searchFilter.releaseBefore = new Date(releaseBefore);
 		if (releaseAfter) searchFilter.releaseAfter = new Date(releaseAfter);
-		if (playtime) searchFilter.playtime = playtime;
+		if (playtimeMax) searchFilter.playtimeMax = playtimeMax;
+		if (playtimeMin) searchFilter.playtimeMin = playtimeMin;
 		if (playerCount) searchFilter.playerCount = playerCount;
 		if (owner) searchFilter.owner = owner;
 		postData(searchFilter);
@@ -64,13 +67,15 @@ const GamesList: FC<GamesListProps> = () => {
 				setPlatform={setPlatform}
 				setReleaseBefore={setReleaseBefore}
 				setReleaseAfter={setReleaseAfter}
-				setPlaytime={setPlaytime}
+				setPlaytimeMax={setPlaytimeMax}
+				setPlaytimeMin={setPlaytimeMin}
 				setPlayerCount={setPlayerCount}
 				setOwner={setOwner}
 				platform={platform}
 				releaseAfter={releaseAfter}
 				releaseBefore={releaseBefore}
-				playtime={playtime}
+				playtimeMax={playtimeMax}
+				playtimeMin={playtimeMin}
 				playerCount={playerCount}
 				owner={owner}
 				filterFunction={search}
