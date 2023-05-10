dacc41ddf6d7aec9fc036a14cea55bbef0725e00
Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>
2023-05-10 12:44:28 +0200

    Updated LoginStatus to use React Query

---
+++ frontend/src/hooks/api/auth.ts
@@ -0,0 +1,48 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import axios, { AxiosError } from 'axios';
+
+interface User {
+	cid: string;
+	nick: string;
+	firstName: string;
+	lastName: string;
+
+	email: string;
+	phone: string;
+	avatarUrl?: string;
+	acceptanceYear: number;
+	gdpr: boolean;
+	language: string;
+	authorities: [{ id: string; authority: string }];
+	//	groups: GammaGroup[];
+	websiteURLs?: string;
+	//Internal extension
+	isSiteAdmin: boolean;
+}
+
+export const useUser = () => {
+	return useQuery<User | null, AxiosError>({
+		queryKey: ['user'],
+		queryFn: async () => {
+			try {
+				return await axios.get('/api/v1/auth/user').then((res) => res.data);
+			} catch (error) {
+				const err = error as AxiosError;
+				if (err.response?.status === 401) return null;
+
+				throw err;
+			}
+		}
+	});
+};
+
+export const useLogout = () => {
+	const queryClient = useQueryClient();
+
+	return useMutation({
+		mutationFn: () => axios.post('/api/v1/auth/logout'),
+		onSuccess: () => {
+			queryClient.invalidateQueries(['user']);
+		}
+	});
+};
