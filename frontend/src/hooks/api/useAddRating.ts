73233712d788616ada4fe03513fe06376bdb37bc
Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>
2023-05-10 13:35:16 +0200

    Added query invalidation

--- frontend/src/hooks/api/useAddRating.ts
+++ frontend/src/hooks/api/useAddRating.ts
@@ -1,9 +1,14 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
 
 export const useAddRating = () => {
+	const queryClient = useQueryClient();
+
 	return useMutation({
 		mutationFn: (data: { game: string; rating: number }) =>
-			axios.post('/api/v1/rating/rate', data)
+			axios.post('/api/v1/rating/rate', data),
+		onSuccess: () => {
+			queryClient.invalidateQueries(['gamesList']);
+		}
 	});
 };
--- frontend/src/hooks/api/useMarkAsPlayed.ts
+++ frontend/src/hooks/api/useMarkAsPlayed.ts
@@ -1,9 +1,14 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
 
 export const useMarkAsPlayed = () => {
+	const queryClient = useQueryClient();
+
 	return useMutation({
 		mutationFn: (data: { gameId: string }) =>
-			axios.post('/api/v1/games/markAsPlayed', data)
+			axios.post('/api/v1/games/markPlayed', data),
+		onSuccess: () => {
+			queryClient.invalidateQueries(['gamesList']);
+		}
 	});
 };
