6513bf18b948625704c5091c69e4fecb50ead589
Oscar Eriksson <oscariremma@gmail.com>
2023-05-16 15:30:26 +0200

    Some more refactoring and added feedback to request responses

--- frontend/src/hooks/api/borrow.ts
+++ frontend/src/hooks/api/borrow.ts
@@ -1,7 +1,24 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import axios, { AxiosError } from 'axios';
 
+export const useBorrowRequestList = () => {
+	return useQuery<
+		{
+			gameId: string;
+			name: string;
+			user: string;
+			borrowStart: string;
+			borrowEnd: string;
+		}[],
+		AxiosError
+	>(['borrowRequestList'], () =>
+		axios.get('/api/v1/borrow/request/list').then((res) => res.data)
+	);
+};
+
 export const useBorrowRequest = () => {
+	const queryClient = useQueryClient();
+
 	return useMutation<
 		unknown,
 		AxiosError,
@@ -12,8 +29,49 @@ export const useBorrowRequest = () => {
 		}
 	>({
 		mutationFn: async (data) =>
-			axios.post('/api/v1/borrow/request', data).then((res) => res.data)
+			axios.post('/api/v1/borrow/request', data).then((res) => res.data),
+		onSuccess: () => {
+			queryClient.invalidateQueries(['borrowRequestList']);
+		}
 	});
+};
+
+export const useBorrowRequestRespond = () => {
+	const queryClient = useQueryClient();
+
+	return useMutation<
+		unknown,
+		AxiosError,
+		{
+			gameId: string;
+			startDate: string;
+			endDate: string;
+			approved: boolean;
+		}
+	>({
+		mutationFn: async (data) =>
+			axios
+				.post('/api/v1/borrow/request/respond', data)
+				.then((res) => res.data),
+		onSuccess: () => {
+			queryClient.invalidateQueries(['borrowRequestList']);
+			queryClient.invalidateQueries(['borrowsList']);
+		}
+	});
+};
+
+export const useBorrowsList = () => {
+	return useQuery<
+		{
+			gameName: string;
+			user: string;
+			borrowStart: string;
+			borrowEnd: string;
+		}[],
+		AxiosError
+	>(['borrowsList'], () =>
+		axios.get('/api/v1/borrow/list').then((res) => res.data)
+	);
 };
 
 export const useBorrow = () => {
